50eedd895663bfad17933a1d48143b308b6739cb
KhushiJhala <81377199+KhushiJhala@users.noreply.github.com>
2021-07-09 10:07:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/UserRoute"); 
+const adminRoute = require("./Routes/AdminRoute");
+const hotelRoute = require("./Routes/HotelRoute");
+const bookingRoute = require("./Routes/BookingRoute");
+const cors = require("cors"); 
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors()); 
+
+mongoose.connect("mongodb://localhost:27017/Hotel_Bookings" , {
+    useUnifiedTopology : true ,
+    useNewUrlParser : true,
+})
+.then(() => {
+    console.log("mongodb connection succesful");
+})
+.catch(() => {
+    console.error("mongodb connection unsuccessful");
+});
+
+app.use(userRoute);
+app.use(adminRoute);
+app.use(hotelRoute);
+app.use(bookingRoute);
+const PORT = 8080;
+
+app.listen(PORT , () => {
+    console.log(`My server is running on the port ${PORT}`);
+})
